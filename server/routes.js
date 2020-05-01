87d2e6c7c9000672501981c9f5192cd03cfb9356
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-01 17:33:03 +1200

    Refactor code

--- server/routes.js
+++ server/routes.js
@@ -143,25 +143,21 @@ router.get('/all', (req, res) => {
 })
 
 // filters the animals array and returns the object that matches the id params
-// router.get('/:id', (req, res) => {
-//   const { id } = req.params
-//   const animal = animals.filter(animalObject => {
-//     return animalObject.id === Number(id)
-//   })
-//   res.json(
-//     animal[0]
-//   )
-// })
-
-// returns the first item in the array that has a matching id with the params
 router.get('/:id', (req, res) => {
   const { id } = req.params
-  const animal = animals.reduce((ac, cv) =>
-    ac.id === Number(id) ? ac : cv
-  )
-  res.json(
-    animal
-  )
+  const animal = animals.filter(animalObject => {
+    return animalObject.id === Number(id)
+  })
+  res.json(animal[0])
 })
+
+// returns the first item in the array that has a matching id with the params
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params
+//   const animal = animals.reduce((ac, cv) =>
+//     ac.id === Number(id) ? ac : cv
+//   )
+//   res.json(animal)
+// })
 
 module.exports = router
