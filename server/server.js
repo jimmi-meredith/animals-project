3fb1b7198c0b8e0591e8d5af65b8092f260158e4
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-05-04 18:32:28 +1200

    Add client files

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const animalsroutes = require('./routes')
+const path = require('path')
 
 // a function that returns an object that represents the entire server
 const server = express()
@@ -10,5 +11,11 @@ server.use(express.json())
 // connecting the route to the server
 // this is the prefix for all animals routes
 server.use('/animals', animalsroutes)
+
+server.use(express.static(path.join(__dirname, './public')))
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 module.exports = server
